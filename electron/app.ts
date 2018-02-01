90882aac35a4ea017957e63f31239343e1fb7b97
Michael Gardner <mikejr83@gmail.com>
2018-02-01 14:09:22 -0500

    Split out startup function
    Changing how the Electron app bootstraps. I wasn't too fond of dumping
    everything into one file. Handle switches in the main and app
    bootstrapping in another.

---
+++ electron/app.ts
@@ -0,0 +1,99 @@
+import {
+  app,
+  BrowserWindow,
+  Menu,
+  screen
+} from "electron";
+import path = require("path");
+import url = require("url");
+import * as yargs from "yargs";
+
+import logger from "./logging";
+
+// import { buildMenu } from "./menu/builder";
+
+// import * as log from "electron-log";
+
+export default class App {
+  public static AppWindow: Electron.BrowserWindow;
+  public static application: Electron.App;
+  public static BrowserWindow;
+  /**
+   *
+   *
+   * @static
+   * @param {Electron.App} electronApp
+   * @param {typeof BrowserWindow} browserWindow
+   * @memberof App
+   */
+  public static main() {
+    /**
+     * We pass the Electron.App object and the Electron.BrowserWindow into this function
+     * so this class1 has no dependencies.  This makes the code easier to write tests for
+     */
+
+    App.BrowserWindow = BrowserWindow;
+    App.application = app;
+    App.application.on("window-all-closed", App.onWindowAllClosed);
+    App.application.on("ready", App.onReady);
+  }
+
+  private static onWindowAllClosed() {
+    if (process.platform !== "darwin") {
+      App.application.quit();
+    }
+  }
+
+  private static onClose() {
+    App.AppWindow = null;
+  }
+
+  private static onReady() {
+    logger.debug("onReady");
+
+    // Create the App window
+    App.createWindow();
+
+    App.loadPage("../index.html");
+
+    // Get command line args.
+    const serve = yargs.argv.serve !== undefined;
+    // Open the DevTools.
+    if (serve) {
+      App.AppWindow.webContents.openDevTools();
+    }
+
+    // buildMenu();
+
+    App.AppWindow.on("closed", App.onClose);
+  }
+
+  private static createWindow() {
+    logger.debug("Beginning window creation.");
+
+    const electronScreen = screen;
+    const size = electronScreen.getPrimaryDisplay().workAreaSize;
+
+    logger.debug("Creating a new BrowserWindow with the {0} height and {1} width", size.height, size.width);
+    // Create the browser window.
+    App.AppWindow = new BrowserWindow({
+      height: size.height,
+      width: size.width,
+      x: 0,
+      y: 0
+    });
+  }
+
+  private static loadPage(pathname: string) {
+    pathname = path.join(__dirname, pathname);
+
+    logger.debug("Looking for start page:", path.resolve(pathname));
+
+    // and load the index.html of the app.
+    App.AppWindow.loadURL(url.format({
+      pathname,
+      protocol: "file:",
+      slashes: true
+    }));
+  }
+}
--- main.ts
+++ main.ts
@@ -1,80 +1,19 @@
 import { app, BrowserWindow, screen } from "electron";
 import * as electronReload from "electron-reload";
 import * as path from "path";
+import * as yargs from "yargs";
 
+import App from "./electron/app";
 import logger from "./electron/logging";
 
-let win, serve;
-const args = process.argv.slice(1);
-serve = args.some(val => val === "--serve");
-import * as url from "url";
-
-if (serve) {
+if (yargs.argv.serve) {
+  logger.debug("Starting electron-reload.");
   electronReload(__dirname, {});
 }
 
-function createWindow() {
-  logger.debug("Beginning window creation.");
-
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
-
-  logger.debug("Creating a new BrowserWindow with the {0} height and {1} width", size.height, size.width);
-  // Create the browser window.
-  win = new BrowserWindow({
-    height: size.height,
-    width: size.width,
-    x: 0,
-    y: 0
-  });
-
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, "/index.html"),
-    protocol: "file:",
-    slashes: true
-  }));
-
-  // Open the DevTools.
-  if (serve) {
-    win.webContents.openDevTools();
-  }
-
-  // Emitted when the window is closed.
-  win.on("closed", () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-}
-
 try {
-
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on("ready", createWindow);
-
-  // Quit when all windows are closed.
-  app.on("window-all-closed", () => {
-    logger.info("window-all-closed firing! This means the app should be shutting down.");
-
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== "darwin") {
-      app.quit();
-    }
-  });
-
-  app.on("activate", () => {
-    // On OS X it"s common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
-  });
-
+  logger.silly("Starting App.main...");
+  App.main();
 } catch (e) {
   // Catch Error
   logger.error("An error was generated during the setup of application event handling.", e);
