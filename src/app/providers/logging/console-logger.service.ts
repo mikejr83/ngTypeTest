adffb7d2d85efce595215ad6d0afdd12556e839d
Michael Gardner <mikejr83@gmail.com>
2018-02-09 10:37:59 -0500

    Update Documentation for Logging Provider

--- src/app/providers/logging/console-logger.service.ts
+++ src/app/providers/logging/console-logger.service.ts
@@ -5,13 +5,35 @@ import { Logger } from "./logger.service";
 
 const noop = (): any => undefined;
 
+/**
+ * Console logger implementation.
+ *
+ * This implementation of a logger creates a provider which sends any log messages to the browser's console.
+ *
+ * @export
+ * @class ConsoleLoggerService
+ * @implements {Logger}
+ */
 @Injectable()
 export class ConsoleLoggerService implements Logger {
 
+  /**
+   * Creates an instance of ConsoleLoggerService.
+   * @param {ConfigurationService} configurationService
+   * @memberof ConsoleLoggerService
+   */
   constructor(private configurationService: ConfigurationService) {
     this.configurationService.loadConfig();
   }
 
+  /**
+   * Log a debug message to the console.
+   *
+   * Uses console.debug.
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   get debug() {
     if (this.findLevel() >= 0) {
       return console.debug.bind(console);
@@ -20,6 +42,14 @@ export class ConsoleLoggerService implements Logger {
     }
   }
 
+  /**
+   * Log an informational message to the console.
+   *
+   * Uses console.info
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   get info() {
     if (this.findLevel() >= 2) {
       return console.info.bind(console);
@@ -28,6 +58,14 @@ export class ConsoleLoggerService implements Logger {
     }
   }
 
+  /**
+   * Log a warning message to the console.
+   *
+   * Uses console.warn
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   get warn() {
     if (this.findLevel() >= 3) {
       return console.warn.bind(console);
@@ -36,6 +74,14 @@ export class ConsoleLoggerService implements Logger {
     }
   }
 
+  /**
+   * Log an error to the console.
+   *
+   * Uses console.error
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   get error() {
     if (this.findLevel() >= 4) {
       return console.error.bind(console);
@@ -44,7 +90,16 @@ export class ConsoleLoggerService implements Logger {
     }
   }
 
+
+  /**
+   * Gets the current logging level from the configuration.
+   *
+   * @private
+   * @returns
+   * @memberof ConsoleLoggerService
+   */
   private findLevel() {
+    // If we have no configuration return a default of 2 (info).
     if (!this.configurationService.configuration) {
       return 2;
     }
--- src/app/providers/logging/logger.service.ts
+++ src/app/providers/logging/logger.service.ts
@@ -1,25 +1,79 @@
 import { Injectable } from "@angular/core";
 
 /**
- *
+ * Provides the blueprint for any logging service
  *
  * @export
  * @abstract
  * @class Logger
  */
 export abstract class Logger {
-
+  /**
+   * Log a debug message
+   *
+   * @type {*}
+   * @memberof Logger
+   */
+  debug: any;
+  /**
+   * Log an informational message.
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   info: any;
+  /**
+   * Log a warning.
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   warn: any;
+  /**
+   * Log an error!
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   error: any;
 }
 
+/**
+ * Stub LoggerService. This provides a basic implementation for the logger provider.
+ *
+ * @export
+ * @class LoggerService
+ * @implements {Logger}
+ */
 @Injectable()
 export class LoggerService implements Logger {
-
+  /**
+   * Log a debug message
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   debug: any;
+  /**
+   * Log an informational message.
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   info: any;
+  /**
+   * Log a warning.
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   warn: any;
+  /**
+   * Log an error!
+   *
+   * @type {*}
+   * @memberof Logger
+   */
   error: any;
 
   invokeConsoleMethod(type: string, args?: any): void {}
