bf89ca75b4621d6869135e84fe4e0329311204c5
Michael Gardner <mikejr83@gmail.com>
2018-02-08 21:16:01 -0500

    User documentation

---
+++ electron/user/README.md
@@ -0,0 +1,7 @@
+## User
+
+The user folder contains all modules pertaining to the users.
+
+### User
+
+The user module simply contains the interface for defining the user object's data structure.
--- electron/user/user.ts
+++ electron/user/user.ts
@@ -1,7 +1,31 @@
 import { IUserConfiguration } from "../configuration/user";
 
+/**
+ * Interface for defining the structure of user data.
+ *
+ * @export
+ * @interface IUser
+ */
 export interface IUser {
+  /**
+   * Uniquly identifiable string; email address.
+   *
+   * @type {string}
+   * @memberof IUser
+   */
   username: string;
+  /**
+   * Display name of the user; greeting
+   *
+   * @type {string}
+   * @memberof IUser
+   */
   name: string;
+  /**
+   * User specific configuration.
+   *
+   * @type {IUserConfiguration}
+   * @memberof IUser
+   */
   configuration: IUserConfiguration;
 }
